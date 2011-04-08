074bb373e368a962788ba7f5da0783dd4e14c928
stephband <stephband@webdoc.com>
2011-04-08 11:35:29 +0200

    Adds (//commented) code that helps delegate, handle attributes, and prepare drag feedback. The code needs reviewing before being uncommented.

--- js/jquery.extensions.js
+++ js/jquery.extensions.js
@@ -376,3 +376,291 @@ jQuery.fn.extend({
   };
   
 })(jQuery);
+
+
+// TODO: Review the useful bits of the following...
+
+///**
+// * eventHandler provide function to handle events
+// * @namespace
+// */
+//WebDoc.eventHandler = {
+//  /**
+//   * This function is a helper to define multiple event handler with a kind of configuration file (the handleConfig parameter)
+//   * Add event handler based on handlerConfig parameter. You can add as many event handler you want.
+//   * @param handlerConfig. Definition of event handlers. config has the following structure
+//   * <pre class="code">
+//   *   {
+//   *     'selector to bind on': {
+//   *       'event type': function,                 // direct bind
+//   *       'event_type': {                         // delegation
+//   *         'selector to delegate on': function
+//   *       }
+//   *     }
+//   *   }
+//   * </pre>
+//   */
+//  add: function(handlerConfig){
+//    var selector, elem, events, event, handler, deleSelector, deleHandler;
+//
+//    // Loop through handlers and bind them to their selectors
+//    for ( selector in handlerConfig ){
+//      elem = jQuery( selector === 'document' ? document : selector );
+//      events = handlerConfig[selector];
+//
+//      for ( event in events ){
+//        handler = events[event];
+//
+//        // Either bind the handler...
+//        if ( typeof handler === 'function' ){
+//          elem.bind( event, events[event] );
+//        }
+//        // ...or delegate the list of handlers
+//        else {
+//          for ( deleSelector in handler ){
+//            deleHandler = handler[deleSelector];
+//            elem.delegate( deleSelector, event, deleHandler );
+//          }
+//        }
+//      }
+//    }
+//  },
+//
+//  /**
+//   * function that can be used for dragstart event. This function automatically fills the clipboard based on
+//   * data-mimetypes attribute.
+//   * @param e the dragstart event object
+//   */
+//  prepareDragData: function(e){
+//    var elem = jQuery( e.target ),
+//        data = elem.data('mimetypes'),
+//        mimetype;
+//
+//    if ( !data ) { return; }
+//
+//    for (mimetype in data){
+//      ddd('[drag data] mimetype:', mimetype, 'data:', data[mimetype]);
+//      e.originalEvent.dataTransfer.setData( mimetype, JSON.stringify( data[mimetype] ) );
+//    }
+//
+//    //look if it's a system app, and store the app name in data app_name
+//    if(elem.attr('href') === '#insert-system-app'){
+//      e.originalEvent.dataTransfer.setData( 'app-name', JSON.stringify( elem.data('app-name') ) );
+//    }
+//  },
+//
+//  /**
+//   * function that automatically set drag feedback based on data-feedback attribute.
+//   * @param e the dragstart event object
+//   */
+//  prepareDragFeedback: function(e){
+//    var elem = jQuery( e.target ),
+//      data = elem.data('feedback'),
+//      offset, dragOffset;
+//
+//    if ( data && data.node ){
+//      ddd('[drag feedback] node:', data.node);
+//      jQuery( document.body ).append( data.node );
+//      e.originalEvent.dataTransfer.setDragImage( data.node, data.width || 32, data.height || 32 );
+//    }
+//    else {
+//      offset = elem.offset();
+//      dragOffset = {
+//        offsetX: e.pageX - offset.left,
+//        offsetY: e.pageY - offset.top
+//      };
+//
+//      ddd('[drag feedback] offset:', dragOffset);
+//      e.originalEvent.dataTransfer.setDragImage( e.target, dragOffset.left, dragOffset.top );
+//      e.originalEvent.dataTransfer.setData( 'webdoc/offset', JSON.stringify(dragOffset) );
+//    }
+//  },
+//
+//  /**
+//   * Create a handle function that handle an attribute of the target that trigger the event
+//   * @param attr {String} attribute to check
+//   * @param obj {Object} A Hash that map value to check in the attribute and function to execute.
+//   * @param context {Object} context object that will be used for calling the callback
+//   * @return {Function} a function that will handle event based on attribute.
+//   */
+//  handleAttribute: function( attr, obj, context ){
+//    // Curry link handler using this scope
+//    return function(e){
+//      var link = jQuery(this),
+//          match = link.attr( attr );
+//
+//      // If the attribute matches an obj key
+//      if ( match && obj[match] ) {
+//        ddd( '[Handler] ' + attr + '="' + match + '"' );
+//
+//        // Don't do anything browsery
+//        e.preventDefault();
+//
+//        // Call it with link as scope
+//        return obj[match].call( context || this, e );
+//      }
+//    };
+//  },
+//  
+////  /**
+////   * Create a handler function that handles the data of the current target.
+////   * @param obj {Object} A hash that map value to check in the attribute and function to execute.
+////   * @param context {Object} context object that will be used for calling the callback
+////   * @return {Function} a function that will handle event based on attribute.
+////   */
+////  handleData: function( obj, context ){
+////    // Curry handler using this scope
+////    return function(e){
+////      var currentTarget = jQuery(this),
+////          data = currentTarget.data(),
+////          key, result;
+////      
+////      if (!data) { return; }
+////      
+////      for (key in data) {
+////        if ( obj[key] ){
+////          console.log( '[handleData]', key, data[key] );
+////          
+////          // Don't do anything browsery
+////          e.preventDefault();
+////          
+////          // Call it with link as scope
+////          result = obj[key].call( context || this, e );
+////        }
+////      }
+////      
+////      // Return the last matched function's return value
+////      // or undefined.
+////      return result;
+////    };
+////  },
+//
+//  /**
+//   * Create a handle function that can manage drop event
+//   * @param obj {Object} A hash that map mimetype to an action to perform on drop of this mimetype
+//   * @param context {Object context to use when calling the callback}
+//   * TODO move this outside the common framework
+//   */
+//  handleMimeType: function( obj, context ){
+//    // Curry handler using this scope
+//    return function(e, item_uuid){
+//      
+//      WebDoc.application.boardController.unselectAll();
+//      // This block is just for logging, so we can see some
+//      // data. IE does not have the property dataTransfer.types
+//      // so we can't tell what mimetypes are being carried in IE.
+//      if (window.console && console.log && e.originalEvent.dataTransfer.types) {
+//
+//        var types = e.originalEvent.dataTransfer.types,
+//            l = types.length,
+//            dataObj;
+//
+//        var target = jQuery(e.target),
+//            currentTarget = jQuery(e.currentTarget),
+//            viewAtPoint = WebDoc.application.boardController.getViewAtPoint(e).view;
+//
+//
+//        ggg('[Handler] handleMimeType');
+//
+//        // Log available mimetypes
+//        while (l--) {
+//          dataObj = e.originalEvent.dataTransfer.getData(types[l]);
+//          ddd(types[l], 'data:', dataObj );
+//        }
+//      }
+//      if(!item_uuid && viewAtPoint && viewAtPoint.getWidgetApiObject && viewAtPoint.getWidgetApiObject()){
+//        item_uuid = viewAtPoint.getWidgetApiObject().uuid;
+//      }
+//      if(viewAtPoint && viewAtPoint.getWidgetApiObject && viewAtPoint.getWidgetApiObject()){
+//        ddd('find children target');
+//        if(item_uuid && WebDoc.handlers._dropListeners[item_uuid]){
+//          var currentNode = jQuery(viewAtPoint.itemDomNode).contents();
+//          var position = { left: viewAtPoint.item.getLeft(), top: viewAtPoint.item.getTop() };
+//          for ( element_id in WebDoc.handlers._dropListeners[item_uuid] ){
+//            var element = jQuery(currentNode).find('#'+element_id),
+//                pos = WebDoc.application.boardController.mapToPageCoordinate(e);
+//            if(pos.top < (position.top + element.position().top +  jQuery(element).outerHeight()) && pos.top > (position.top + element.position().top)){
+//              if(pos.left < (position.left + element.position().left + jQuery(element).outerWidth()) && pos.left > (position.left + element.position().left)){
+//                target = element;
+//                break;
+//              }
+//            }
+//          }
+//        }
+//      }else{
+//        target = currentTarget;
+//      }
+//      var type_for_app;
+//      var list_types_to_parse = ['application/x-moz-file-promise-url', 'text/uri-list', 'text/plain',  'application/x-moz-file-promise-url', 'text/x-moz-url' ]
+//      for ( type in obj ){
+//        if ( e.originalEvent.dataTransfer.getData(type) || (type === 'Files' && e.originalEvent.dataTransfer.files && e.originalEvent.dataTransfer.files.length > 0) ) {
+//          ggg( 'Matched mimetype: ' + type );
+//
+//          // Call it with this as scope. If it returns false we want
+//          // to continue trying other mimetypes, otherwise return.
+//          //
+//          type_for_app = type;
+//          if(list_types_to_parse.indexOf(type)> -1){
+//            var data = e.originalEvent.dataTransfer.getData(type);
+//            type_for_app = WebDoc.DragAndDropController.getFileType(data);
+//          }
+//
+//          if(item_uuid && WebDoc.handlers._dropListeners[item_uuid] &&  WebDoc.handlers._dropListeners[item_uuid][jQuery(target).id()] && (WebDoc.handlers._dropListeners[item_uuid][jQuery(target).id()][type_for_app] || WebDoc.handlers._dropListeners[item_uuid][jQuery(target).id()]['*'])){
+//            if('Files' == type){
+//              files = e.originalEvent.dataTransfer.files;
+//              if( files.length > 5 ) {
+//                WebDoc.notificationController.notify("Too many images selected. You may only move 5 images at a time.", WebDoc.NotificationController.WARNING, 0);
+//              }else{
+//                var uploadOptions =  { maxSize: WebDoc.ajaxUploadManager.MAX_SIZE, allowedTypes: WebDoc.ajaxUploadManager.TYPES_IMAGE };
+//                for (var i = 0; i < files.length; i++){
+//                  WebDoc.ajaxUploadManager.upload('/images', 'image[attachment]', files[i], uploadOptions, null, function(data) {
+//                    var data =  {type: 'image', mimetype: 'image', url: data.image.properties.default_url };
+//                    if (!(WebDoc.handlers._dropListeners[item_uuid][jQuery(target).id()][type] && WebDoc.handlers._dropListeners[item_uuid][jQuery(target).id()][type](data) !== false)){
+//                      if (WebDoc.handlers._dropListeners[item_uuid][jQuery(target).id()]['*']) WebDoc.handlers._dropListeners[item_uuid][jQuery(target).id()]['*'](data);
+//                    }
+//                  }.bind(this) );
+//                }
+//              }
+//            }else{
+//              var data = e.originalEvent.dataTransfer.getData(type);
+//              try{ data = JSON.parse(data);}catch(e){};
+//              //if (jQuery.regex.image.test(data)){
+//              //  data = { url: data };
+//              //}
+//              if(list_types_to_parse.indexOf(type)> -1){
+//                type = WebDoc.DragAndDropController.getFileType(data);
+//                data = { url: data };
+//              }
+//              jQuery.extend(data, {mimetype: type});
+//              if (!(WebDoc.handlers._dropListeners[item_uuid][jQuery(target).id()][type] && WebDoc.handlers._dropListeners[item_uuid][jQuery(target).id()][type](data) !== false)){
+//                if (WebDoc.handlers._dropListeners[item_uuid][jQuery(target).id()]['*']) WebDoc.handlers._dropListeners[item_uuid][jQuery(target).id()]['*'](data);
+//              }
+//            }
+//            // Don't do anything browsery
+//            e.preventDefault();
+//            ggge();
+//            break;
+//          } else {
+//            //
+//            if ( obj[type].call( context || this, e ) !== false ) {
+//
+//              // Don't do anything browsery
+//              e.preventDefault();
+//              ggge();
+//              break;
+//            }
+//
+//            ggge();
+//          }
+//        }
+//      }
+//
+//      // To end the group created at the begining of this function
+//      if ( window.console && console.log && e.originalEvent.dataTransfer.types ) {
+//        ggge();
+//      }
+//    };
+//  }
+//
+//};
+
