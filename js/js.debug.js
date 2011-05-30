543426f384c8dfb64edc0fc82790d84520b656ad
stephband <stephband@webdoc.com>
2011-05-30 21:04:23 +0200

    Stylesheet analysis.

--- js/js.debug.js
+++ js/js.debug.js
@@ -48,7 +48,7 @@
 				}
 			}
 		},
-		styles: function() {
+		classes: function() {
 			var stylesheets = document.styleSheets,
 			    l = stylesheets.length,
 			    stylesheet, rules, m, rule;
@@ -83,41 +83,71 @@
 				}
 			}
 			
-			var classNames = Object.keys(classes).sort(function(a, b) {
-				var baseA = /(?:([^_]+)_)?(?:([^_]+)_)?(?:([^_]+)_)?([^_]+)$/.exec(a),
-						baseB = /(?:([^_]+)_)?(?:([^_]+)_)?(?:([^_]+)_)?([^_]+)$/.exec(b);
+			function sortClass(a, b, za, zb) {
+				var baseA = /^(?:([\w\-]+)_)?([^_\:\[]+)(.*)$/.exec(a),
+				    baseB = /^(?:([\w\-]+)_)?([^_\:\[]+)(.*)$/.exec(b),
+				    xa = za || baseA[3],
+				    xb = zb || baseB[3];
 				
-				// Time to get recursive
-				
-				if (baseA[4] === baseB[4]) {
-					if (baseA[3] === baseB[3]) {
-						if (baseA[2] === baseB[2]) {
-							if (baseA[1] === baseB[1]) {
-								
-							}
-							else if (([baseA[1], baseB[1]]).sort()[0] === baseA[1]) { return -1; }
-							else { return 1; }
-						}
-						else if (([baseA[2], baseB[2]]).sort()[0] === baseA[2]) { return -1; }
-						else { return 1; }
+				if (baseA[2] === baseB[2]) {
+					if (baseA[1] && baseB[1]) {
+						return sortClass(baseA[1], baseB[1], xa, xb);
 					}
-					else if (([baseA[3], baseB[3]]).sort()[0] === baseA[3]) { return -1; }
-					else { return 1; }
+					else if (!baseA[1] && !baseB[1]) {
+						if (xa) {
+							if (xb) {
+								if (xa.length > xb.length) {
+									return 1;
+								}
+								else {
+									return -1;
+								}
+							}
+							else {
+								return 1;
+							}
+						}
+						else if (xb) {
+							return -1;
+						}
+					}
+					else if (baseA[1]) {
+						return 1;
+					}
+					else {
+						return -1;
+					}
 				}
-				else if (([baseA[4], baseB[4]]).sort()[0] === baseA[4]) { return -1; }
-				else { return 1; }
+				else if (([baseA[2], baseB[2]]).sort()[0] === baseA[2]) {
+					return -1;
+				}
+				else {
+					return 1;
+				}
 				
 				return 0;
-			}),
+			}
+			
+			// --------- log ---------
+			
+			var classNames = Object.keys(classes).sort(sortClass);
+			
 			l = classNames.length;
 			
 			var logElem = jQuery('<dl/>', {'class': 'css_log log'});
 			
+			jQuery('head').append('<style>'+
+				'.log dt {float:left; clear:both; width: 38%; text-align: right; margin: 0;} ' +
+				'.log dd {float:right; clear:right; width: 60%; margin: 0;} ' +
+				'.log .unfound {opacity: 0.6;} ' +
+			'</style>');
+			
 			while (l--) {
-				logElem.append((function(classNames, l, classes){
+				logElem.prepend((function(classNames, l, classes){
 					var className = classNames[l],
 							rules = classes[className],
-							html = '<dt>'+className+'</dt>',
+							found = jQuery('.'+/^([^\:]+)/.exec(className)[1]),
+							html = '<dt'+(!found.length ? ' class="unfound"' : '')+'>'+className+'</dt>',
 							m = rules.length,
 							rule, files = {};
 					
@@ -135,7 +165,10 @@
 				})(classNames, l, classes));
 			}
 			
+			jQuery('<hr style="clear:both;"/>').appendTo('body');
+			jQuery('<h1 style="width: 38%; text-align: right;">Classes</h1>').appendTo('body');
 			logElem.appendTo('body');
+			jQuery('<hr style="clear:both;"/>').appendTo('body');
 		}
 	};
 })(window);
