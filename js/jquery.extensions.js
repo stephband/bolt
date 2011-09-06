389af2755fd9981d112bbd437f468d6e37b62931
stephband <stephband@googlemail.com>
2011-09-06 19:36:35 +0200

    Adds jQuery.fn.blockScroll() and jQuery.fn.unblockScroll()

--- js/jquery.extensions.js
+++ js/jquery.extensions.js
@@ -419,6 +419,75 @@ jQuery.fn.extend({
 });
 
 
+// jQuery.fn.blockScroll
+// 
+// Prevent scroll events on this node if this node has come
+// to the limit of it's scroll travel.
+//
+// When scrolling a target inside this element, we check all
+// nodes up the DOM tree until we hit the current one, making
+// sure they have all hit their limits before preventing the
+// scroll events.
+
+(function(jQuery, undefined){
+	function preventScroll(e) {
+		var target = e.target,
+		    delta = e.wheelDelta !== undefined ? -e.wheelDelta : e.detail,
+		    scrollTop, scrollMax;
+		
+		if (delta < 0) {
+			// We're trying to scroll up. Climb down the DOM tree
+			// until we hit the current node to see if any nodes
+			// have not yet hit 0.
+			
+			while (target !== e.currentTarget.parentNode) {
+				if (target.scrollTop > 0) {
+					break;
+				}
+				
+				if (target === e.currentTarget) {
+					e.preventDefault();
+					break;
+				}
+				
+				target = target.parentNode;
+			};
+		}
+		else {
+			// We're trying to scroll down. Climb down the DOM
+			// tree until we hit the current node to see if any
+			// nodes are not yet at their lower limit.
+			
+			while (target !== e.currentTarget.parentNode) {
+				scrollNow = target.scrollTop;
+				target.scrollTop = 999999;
+				scrollMax = target.scrollTop;
+				target.scrollTop = scrollNow;
+				
+				if (target.scrollTop < scrollMax) {
+					break;
+				}
+				
+				if (target === e.currentTarget) {
+					e.preventDefault();
+					break;
+				}
+				
+				target = target.parentNode;
+			};
+		}
+	}
+	
+	jQuery.fn.blockScroll = function(){
+		return this.bind('mousewheel DOMMouseScroll', preventScroll);
+	};
+	
+	jQuery.fn.unblockScroll = function(){
+		return this.unbind('mousewheel DOMMouseScroll', preventScroll);
+	};
+})(jQuery);
+
+
 (function(jQuery, undefined){
 	var debug = this.console && console.log;
 	
