ba0f2fd09ecd1cf6c5f8a2b4f1aeab527992e484
stephband <stephband@googlemail.com>
2012-05-09 00:00:58 +0200

    Ah, that was it. classList was failing when no activate classes were defined.

--- js/jquery.event.activate.js
+++ js/jquery.event.activate.js
@@ -150,18 +150,22 @@
 	
 	jQuery(document).ready(function(){
 		var id = window.location.hash,
-		    classlist = '.' + Object.keys(classes).join('.active, .') + '.active';
-		
-		// Setup all things that should start out active. By limiting
-		// this to only special classes, we avoid buttons sending out
-		// activate events.
-		jQuery(classlist).trigger('activate');
-		
+		    classArray = Object.keys(classes),
+		    classList;
+
+		if (classArray.length) {
+			// Setup all things that should start out active. By limiting
+			// this to only special classes, we avoid buttons sending out
+			// activate events.
+			classList = '.' + classArray.join('.active, .') + '.active';
+			jQuery(classList).trigger('activate');
+		}
+
 		// Activate the node that corresponds to the hashref
 		// in the location bar, checking if it's an alphanumeric
 		// id selector (not a hash bang).
 		if (!id || !(/^#[\w\d]/.test(id))) { return; }
-		
+
 		jQuery(id).trigger('activate');
 	});
 })(jQuery);
