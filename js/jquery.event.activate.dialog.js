3f3d1bc2d93c1058b34efb05a9c794a45e4f04c9
stephband <stephband@googlemail.com>
2012-05-09 16:54:44 +0200

    Adds mechanics for slides_dialog, including image max-height rescaling on orientationchange, back and next buttons and activation of the identified slide on dialog activation, as coded for belluard.

--- js/jquery.event.activate.dialog.js
+++ js/jquery.event.activate.dialog.js
@@ -6,14 +6,18 @@
 (function(jQuery, undefined){
 	var docElem = jQuery(document.documentElement),
 
-	    add = jQuery.event.add,
-	   
+	    win = jQuery(window),
+
+	    add = jQuery.event.add,  // elem, types, handler, data, selector
+
 	    remove = jQuery.event.remove,
 
 	    trigger = function(node, type, data) {
 	    	jQuery.event.trigger(type, data, node);
 	    },
 
+	    resizeEvent = ("onorientationchange" in window) ? "orientationchange" : "resize" ,
+	    
 	    count = 0;
 
 	function preventDefault(e) {
@@ -53,6 +57,15 @@
 		remove(document, 'touchmove', preventDefault);
 	}
 
+	function maxHeight(images) {
+		// Add max-height to images. Max height needs to be less than the
+		// window height to account for UI around the slides. TODO: find
+		// a way of making this calculation dynamic.
+		images.css({ maxHeight: win.height() - 180 });
+	}
+
+	// Event handlers
+
 	function click(e){
 		if ( e.currentTarget !== e.target ) { return; }
 		
@@ -76,7 +89,83 @@
 		enableScroll();
 	}
 
+	function prev(e) {
+		var slides = e.data,
+		    slide = slides.filter('.active').prev('.slide');
+
+		if (!slide.length) {
+			slide = slides.find('.slide').last();
+		}
+
+		slide.trigger('activate');
+
+		e.preventDefault();
+	}
+	
+	function next(e) {
+		var slides = e.data,
+		    slide = slides.filter('.active').next('.slide');
+
+		if (!slide.length) {
+			slide = slides.find('.slide').first();
+		}
+
+		slide.trigger('activate');
+
+		e.preventDefault();
+	}
+
+	function rescale(e) {
+		maxHeight(e.data);
+	}
+
 	jQuery.extend(jQuery.event.special.activate.classes, {
+		slides_dialog_layer: {
+			activate: function(e, data, fn) {
+				var slides = jQuery('.slide', e.target),
+				    images = slides.filter('img').add(slides.find('img')),
+				    active, id;
+
+				if (e.relatedTarget) {
+					id = jQuery(e.relatedTarget).data('slide');
+				}
+
+				active = id ? jQuery('#' + id) : slides.filter('.active');
+
+				if (!active.length) {
+					active = slides.first();
+				}
+
+				count++;
+				disableScroll();
+
+				add(e.target, 'click.dialog tap.dialog', click);
+				add(e.target, 'click.dialog tap.dialog', close, 'close', '.close_button');
+				add(e.target, 'click.dialog tap.dialog', prev, slides, '.prev_button');
+				add(e.target, 'click.dialog tap.dialog', next, slides, '.next_button');
+				add(window, resizeEvent + '.' + count, rescale, images);
+
+				data.elem.addClass('notransition');
+				maxHeight(images);
+				active.trigger('activate');
+				data.elem.removeClass('notransition');
+
+				fn();
+			},
+
+			deactivate: function(e, data, fn) {
+				remove(e.target, 'click tap', click);
+				remove(e.target, 'click tap', close);
+				remove(e.target, 'click tap', prev);
+				remove(e.target, 'click tap', next);
+				remove(window, resizeEvent + '.' + count, rescale);
+
+				fn();
+			},
+
+			deactivateend: deactivateend
+		},
+
 		lightbox_dialog_layer: {
 			activate: function(e, data, fn) {
 				add(e.target, 'click.dialog tap.dialog', click);
