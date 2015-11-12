8387ed9f78db41f95e509a7598447fe86df436b8
stephband <stephband@googlemail.com>
2015-11-12 23:41:51 +0100

    Adds keydown[x] and keyup[x] special events

--- js/jquery.event.keys.js
+++ js/jquery.event.keys.js
@@ -17,90 +17,123 @@
 		module(jQuery);
 	}
 })(function(jQuery, undefined){
-	var debug = false,
+	var debug = false;
 
-	    add = jQuery.event.add,
-	    remove = jQuery.event.remove,
-	    trigger = function(node, type, data, handlersOnly) {
-	    	jQuery.event.trigger(type, data, node, handlersOnly);
-	    },
+	var add = jQuery.event.add;
+	var remove = jQuery.event.remove;
+	var trigger = function(node, type, data, handlersOnly) {
+			jQuery.event.trigger(type, data, node, handlersOnly);
+		};
 
-	    nodes = new WeakMap(),
+	var nodesUp = new WeakMap();
+	var nodesDown = new WeakMap();
 
-	    keymap = {
-	    	8:  'backspace',
-	    	9:  'tab',
-	    	13: 'enter',
-	    	16: 'shift',
-	    	17: 'ctrl',
-	    	18: 'alt',
-	    	27: 'escape',
-	    	32: 'space',
-	    	37: 'left',
-	    	38: 'up',
-	    	39: 'right',
-	    	40: 'down',
-	    	46: 'delete',
-	    	48: '0',
-	    	49: '1',
-	    	50: '2',
-	    	51: '3',
-	    	52: '4',
-	    	53: '5',
-	    	54: '6',
-	    	55: '7',
-	    	56: '8',
-	    	57: '9',
-	    	65: 'a',
-	    	66: 'b',
-	    	67: 'c',
-	    	68: 'd',
-	    	69: 'e',
-	    	70: 'f',
-	    	71: 'g',
-	    	72: 'h',
-	    	73: 'i',
-	    	74: 'j',
-	    	75: 'k',
-	    	76: 'l',
-	    	77: 'm',
-	    	78: 'n',
-	    	79: 'o',
-	    	80: 'p',
-	    	81: 'q',
-	    	82: 'r',
-	    	83: 's',
-	    	84: 't',
-	    	85: 'u',
-	    	86: 'v',
-	    	87: 'w',
-	    	88: 'x',
-	    	89: 'y',
-	    	90: 'z'
-	    },
+	var keymap = {
+			8:  'backspace',
+			9:  'tab',
+			13: 'enter',
+			16: 'shift',
+			17: 'ctrl',
+			18: 'alt',
+			27: 'escape',
+			32: 'space',
+			37: 'left',
+			38: 'up',
+			39: 'right',
+			40: 'down',
+			46: 'delete',
+			48: '0',
+			49: '1',
+			50: '2',
+			51: '3',
+			52: '4',
+			53: '5',
+			54: '6',
+			55: '7',
+			56: '8',
+			57: '9',
+			65: 'a',
+			66: 'b',
+			67: 'c',
+			68: 'd',
+			69: 'e',
+			70: 'f',
+			71: 'g',
+			72: 'h',
+			73: 'i',
+			74: 'j',
+			75: 'k',
+			76: 'l',
+			77: 'm',
+			78: 'n',
+			79: 'o',
+			80: 'p',
+			81: 'q',
+			82: 'r',
+			83: 's',
+			84: 't',
+			85: 'u',
+			86: 'v',
+			87: 'w',
+			88: 'x',
+			89: 'y',
+			90: 'z',
+			91: 'cmd'
+		};
 
-	    obj = {
-	    	setup: function(data, namespaces, eventHandle) {
-	    		var n = nodes.get(this) || 0;
+	var keydownDef = {
+		setup: function(data, namespaces, eventHandle) {
+			var n = nodesDown.get(this) || 0;
 
-	    		// Setup this node once
-	    		if (n) { return true; }
+			// Setup this node once
+			if (n) { return true; }
 
-	    		add(this, 'keyup', keyup);
-	    		nodes.set(this, ++n);
-	    		return true;
-	    	},
-	    	
-	    	teardown: function(namespaces) {
-	    		var n = nodes.get(this) || 0;
+			add(this, 'keydown', keydown);
+			nodesDown.set(this, ++n);
+			return true;
+		},
+		
+		teardown: function(namespaces) {
+			var n = nodesDown.get(this) || 0;
 
-	    		remove(this, 'keyup', keyup);
+			remove(this, 'keydown', keydown);
+			if (n < 2) { nodesDown.delete(this); }
+			else { nodesDown.set(this, --n); }
+			return true;
+		}
+	};
 
-	    		if (n < 2) { nodes.delete(this); }
-	    		else { nodes.set(this, --n); }
-	    		return true;
-	    	}
-	    };
+	var keyupDef = {
+		setup: function(data, namespaces, eventHandle) {
+			var n = nodesUp.get(this) || 0;
+
+			// Setup this node once
+			if (n) { return true; }
+
+			add(this, 'keyup', keyup);
+			nodesUp.set(this, ++n);
+			return true;
+		},
+		
+		teardown: function(namespaces) {
+			var n = nodesUp.get(this) || 0;
+
+			remove(this, 'keyup', keyup);
+			if (n < 2) { nodesUp.delete(this); }
+			else { nodesUp.set(this, --n); }
+			return true;
+		}
+	};
+
+	function keydown(e) {
+		var key = keymap[e.keyCode];
+
+		if (debug) console.log('[jquery.event.keys]', e.keyCode, key);
+
+		// Reuse the keydown event as the keydown[x] event
+		e.type = e.type + '[' + key + ']';
+		trigger(e.target, e, null, true);
+	}
 
 	function keyup(e) {
 		var key = keymap[e.keyCode];
@@ -108,13 +141,14 @@
 		if (debug) console.log('[jquery.event.keys]', e.keyCode, key);
 
 		// Reuse the keyup event as the key[x] event
-		e.type = 'key[' + key + ']';
+		e.type = e.type + '[' + key + ']';
 		trigger(e.target, e, null, true);
 	}
 
 	var code;
 
 	for (code in keymap) {
-		jQuery.event.special['key[' + keymap[code] + ']'] = obj;
+		jQuery.event.special['keydown[' + keymap[code] + ']'] = keydownDef;
+		jQuery.event.special['keyup[' + keymap[code] + ']'] = keyupDef;
 	}
 });
