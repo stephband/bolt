08c137170f3b736be99c6c67d886b608d2c88b1c
stephband <stephband@googlemail.com>
2017-11-29 16:55:47 +0100

    Grid overlay

--- debug/grid.js
+++ debug/grid.js
@@ -5,10 +5,10 @@
 	+ '<input type="checkbox" id="grid-debug-input" />'
 	+ '<label class="debug-switch switch" for="grid-debug-input">Grid</label>'
 	+ '<div class="debug-grid-block grid-block block" id="grid-debug-block">'
-	+   '<div class="block grid-1/4"></div>'
-	+   '<div class="block grid-1/4"></div>'
-	+   '<div class="block grid-1/4"></div>'
-	+   '<div class="block grid-1/4"></div>'
+	+   '<div class="block grid-1/4 @1-grid-1/4"></div>'
+	+   '<div class="block grid-1/4 @1-grid-1/4"></div>'
+	+   '<div class="block grid-1/4 @1-grid-1/4"></div>'
+	+   '<div class="block grid-1/4 @1-grid-1/4"></div>'
 	+ '</div>';
 
 	var ready = new Promise(function(accept, reject) {
