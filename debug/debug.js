662098396d330b78262f3167b58da3bb4927181a
stephband <stephband@googlemail.com>
2018-10-16 03:18:25 +0200

    Debug

--- debug/debug.js
+++ debug/debug.js
@@ -4,15 +4,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
     for (node of document.querySelectorAll('p')) {
         if (/^\s*$/.test(node.textContent)) {
-            node.classList.add('debug-border');
+            node.classList.add('debug');
             node.setAttribute('title', 'Are you sure this paragraph should be empty?');
         }
     }
 
     for (node of document.querySelectorAll('li')) {
         if (/^\s*$/.test(node.textContent)) {
-            node.classList.add('debug-border');
-            node.setAttribute('title', 'Are you sure this paragraph should be empty?');
+            node.classList.add('debug');
+            node.setAttribute('title', 'Are you sure this li should be empty?');
         }
     }
 });
