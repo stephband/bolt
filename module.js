f86055a7dceae919b08abb23acd8165e29de58d5
stephband <stephband@googlemail.com>
2020-04-26 19:08:27 +0200

    Tweaks

--- module.js
+++ module.js
@@ -1,1 +1,7 @@
+
+import '../dom/js/device.js';
+import '../dom/js/toggleable.js';
+import '../dom/js/popable.js';
+import '../dom/js/locateable.js';
 import './components/switch-label.js';
+import './components/slides.js';
