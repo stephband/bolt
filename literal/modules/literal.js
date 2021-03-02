8901e32eb7c1163ce5e89fd7515da6e3d0432288
stephband <stephband@googlemail.com>
2021-03-02 11:31:07 +0100

    Literal

--- literal/modules/literal.js
+++ literal/modules/literal.js
@@ -131,14 +131,14 @@ export default function Literal(params, template, source) {
     if (DEBUG) {
         logCompile(source, library, params, names);
         // scope, paramString, code, context, name
-        try { fn = compileAsyncFn(library, 'render, include, imports, documentation', code); }
+        try { fn = compileAsyncFn('render, include, imports, documentation', code, library); }
         catch(e) {
             logError(source, template, e);
             throw e;
         }
     }
     else {
-        fn = compileAsyncFn(library, 'render, include, imports, documentation', code);
+        fn = compileAsyncFn('render, include, imports, documentation', code, library);
     }
 
     // Store a reference to the global object
