ba8488f0f8d2be6699c40f87d86aa39172084926
stephband <stephband@googlemail.com>
2021-04-12 20:38:53 +0200

    Docs

--- elements/auto-toggle.js
+++ elements/auto-toggle.js
@@ -18,10 +18,14 @@ element and upgrades instances already in the DOM.
    python orientation semi colon incident. Duis aute irure indent tabs or spaces 
    velit esse cilium buntum how crunchy duntum. Excepteur tranquilis syntax 
    error memorandum qui officia nostrud operating system alertus. Hyper text 
-   linkus operari avec computatrum ad coniungere hominum. Standards code est 
-   pulchra on chromium et foxus sed souvent suckum in internet explorum.
+   linkus operari avec computatrum ad coniungere hominum.
 </auto-toggle>
 ```
+
+An `auto-toggle` is collapsed by default to it's own `max-height`. A `max-height`
+*must* be set for `auto-toggle` to collapse! When toggled open, that `max-height` 
+is overridden to include the whole height of the content, and the state is 
+transitioned from closed to open via CSS.
 **/
 
 import element     from '../../dom/modules/element.js';
