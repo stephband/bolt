5ad585ef760855ed4cd09bd908efca9afe29582d
stephband <stephband@googlemail.com>
2021-05-05 15:23:45 +0200

    Adds --bleed to overflow-toggle, allowing negative margins to bleed content

--- elements/overflow-toggle.js
+++ elements/overflow-toggle.js
@@ -47,13 +47,20 @@ const config = {
 
 /* Element */
 
-function update(shadow, button, scrollHeight, maxHeight, state) {
+function update(shadow, button, scrollHeight, maxHeight, slot, state) {
     if (scrollHeight > maxHeight) {
-        if (!state) { shadow.appendChild(button); }
+        if (!state) {
+            shadow.appendChild(button);
+            slot.classList.add('buttoned');
+        }
         return true;
     }
-    
-    if (state) { button.remove(); }
+
+    if (state) {
+        button.remove();
+        slot.classList.remove('buttoned');
+    }
+
     return false;
 }
 
@@ -97,11 +104,11 @@ element('overflow-toggle', {
         const maxHeight = parseValue(view.maxHeight || style['max-height'] || 0);
         
         // maxHeight is cached on view when element is open
-        var state = update(shadow, button, slot.scrollHeight, maxHeight, false);
+        var state = update(shadow, button, slot.scrollHeight, maxHeight, slot, false);
         events('resize', window)
         .each((e) => {
             const maxHeight = parseValue(view.maxHeight || style['max-height'] || 0);
-            state = update(shadow, button, slot.scrollHeight, maxHeight, state)
+            state = update(shadow, button, slot.scrollHeight, maxHeight, slot, state)
         });
     },
 
