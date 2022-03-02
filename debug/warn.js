ad1434fea6bdb0ac5ba46c6f921d64a118203e44
Marco Bonetti <marco@cruncher.ch>
2022-03-02 12:03:41 +0100

    empty b

--- debug/warn.js
+++ debug/warn.js
@@ -6,7 +6,7 @@ import { select }    from '../../dom/modules/select.js';
 merge([{}], mutations('text', document.body))
 .each((mutation) => {
     /* Select empty typographic elements and add a warning class */
-    select('p, h1, h2, h3, h4, h5, h6, ul, ol, li, dl, dt, dd, em, blockquote, strong', document.body)
+    select('b, p, h1, h2, h3, h4, h5, h6, ul, ol, li, dl, dt, dd, em, blockquote, strong', document.body)
     .filter((element) => /^\s*$/.test(element.textContent))
     .forEach((element) => element.classList.add('empty-warning'));
 });
