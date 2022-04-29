b0cfad54812646e5f9cd37050f12bdd0242715a9
Stephen <stephband@cruncher.ch>
2022-04-29 17:12:36 +0200

    Submittable

--- attributes/data-submittable.js
+++ attributes/data-submittable.js
@@ -26,13 +26,11 @@ const match = matches('[submittable], [data-submittable]');
 
 // Functions
 events('submit', document)
-.tap(function(value) {
-    console.log(value);
-})
 .filter(compose(match, get('target')))
-.tap(preventDefault)
-.map(get('target'))
 .each(function(form) {
+	e.preventDefault();
+
+	const form = e.target;
 	const method   = form.method;
 	const url      = form.action || '';
     // Allow other values for enctype by reading the attribute first
